901dcba7a956a71aa008faca981ab2ddca9c21bb
jim <irawan.jemmy@gmail.com>
2020-10-01 12:07:58 +0800

    init

---
+++ scripts/Editor/WowNode.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Wowsome {
+  namespace Node {
+    public class NodeConfig {
+      public GUIStyle NodeStyle;
+      public GUIStyle SelectedNodeStyle;
+      public GUIStyle InPointStyle;
+      public GUIStyle OutPointStyle;
+
+      public NodeConfig() {
+        var nodeBorder = new RectOffset(12, 12, 12, 12);
+
+        NodeStyle = new GUIStyle();
+        NodeStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node1.png") as Texture2D;
+        NodeStyle.border = nodeBorder;
+
+        SelectedNodeStyle = new GUIStyle();
+        SelectedNodeStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node1 on.png") as Texture2D;
+        SelectedNodeStyle.border = nodeBorder;
+
+        InPointStyle = new GUIStyle();
+        InPointStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/btn left.png") as Texture2D;
+        InPointStyle.active.background = EditorGUIUtility.Load("builtin skins/darkskin/images/btn left on.png") as Texture2D;
+        InPointStyle.border = new RectOffset(4, 4, 12, 12);
+
+        OutPointStyle = new GUIStyle();
+        OutPointStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/btn right.png") as Texture2D;
+        OutPointStyle.active.background = EditorGUIUtility.Load("builtin skins/darkskin/images/btn right on.png") as Texture2D;
+        OutPointStyle.border = new RectOffset(4, 4, 12, 12);
+      }
+    }
+
+    public abstract class WowNode {
+      public Rect rect;
+      public string title;
+      public bool isDragged;
+      public bool isSelected;
+
+      protected INodeEditor _controller;
+      protected Vector2 _size = new Vector2(200f, 200f);
+
+      public WowNode(INodeEditor controller, Vector2 position) {
+        _controller = controller;
+        rect = new Rect(position.x, position.y, _size.x, _size.y);
+      }
+
+      public void Drag(Vector2 delta) {
+        rect.position += delta;
+      }
+
+      public void Draw() {
+        GUI.BeginGroup(rect);
+        GUILayout.BeginVertical("box", GUILayout.Height(200f));
+        DrawContent();
+        GUILayout.EndVertical();
+        GUI.EndGroup();
+      }
+
+      public abstract void DrawContent();
+
+      public bool ProcessEvents(Event e) {
+        switch (e.type) {
+          case EventType.MouseDown:
+            if (e.button == 0) {
+              if (rect.Contains(e.mousePosition)) {
+                isDragged = true;
+                GUI.changed = true;
+                isSelected = true;
+              } else {
+                GUI.changed = true;
+                isSelected = false;
+              }
+            }
+
+            if (e.button == 1 && isSelected && rect.Contains(e.mousePosition)) {
+              ProcessContextMenu();
+              e.Use();
+            }
+            break;
+
+          case EventType.MouseUp:
+            isDragged = false;
+            break;
+
+          case EventType.MouseDrag:
+            if (e.button == 0 && isDragged) {
+              Drag(e.delta);
+              e.Use();
+              return true;
+            }
+            break;
+        }
+
+        return false;
+      }
+
+      void ProcessContextMenu() {
+        GenericMenu genericMenu = new GenericMenu();
+        genericMenu.AddItem(new GUIContent("Remove node"), false, () => _controller.OnRemoveNode?.Invoke(this));
+        genericMenu.ShowAsContext();
+      }
+    }
+
+    public enum ConnectionPointType { In, Out }
+
+    public class NodeConnectionPoint {
+      public Rect rect;
+
+      public ConnectionPointType type;
+
+      public WowNode node;
+
+      public GUIStyle style;
+
+      public Action<NodeConnectionPoint> OnClickConnectionPoint;
+
+      public NodeConnectionPoint(WowNode node, ConnectionPointType type, GUIStyle style, Action<NodeConnectionPoint> OnClickConnectionPoint) {
+        this.node = node;
+        this.type = type;
+        this.style = style;
+        this.OnClickConnectionPoint = OnClickConnectionPoint;
+        rect = new Rect(0, 0, 10f, 20f);
+      }
+
+      public void Draw() {
+        rect.y = node.rect.y + (node.rect.height * 0.5f) - rect.height * 0.5f;
+
+        switch (type) {
+          case ConnectionPointType.In:
+            rect.x = node.rect.x - rect.width + 8f;
+            break;
+
+          case ConnectionPointType.Out:
+            rect.x = node.rect.x + node.rect.width - 8f;
+            break;
+        }
+
+        if (GUI.Button(rect, "", style)) {
+          if (OnClickConnectionPoint != null) {
+            OnClickConnectionPoint(this);
+          }
+        }
+      }
+    }
+
+    public class NodeConnection {
+      public NodeConnectionPoint inPoint;
+      public NodeConnectionPoint outPoint;
+      public Action<NodeConnection> OnClickRemoveConnection;
+
+      public NodeConnection(NodeConnectionPoint inPoint, NodeConnectionPoint outPoint, Action<NodeConnection> OnClickRemoveConnection) {
+        this.inPoint = inPoint;
+        this.outPoint = outPoint;
+        this.OnClickRemoveConnection = OnClickRemoveConnection;
+      }
+
+      public void Draw() {
+        Handles.DrawBezier(
+            inPoint.rect.center,
+            outPoint.rect.center,
+            inPoint.rect.center + Vector2.left * 50f,
+            outPoint.rect.center - Vector2.left * 50f,
+            Color.white,
+            null,
+            2f
+        );
+
+        if (Handles.Button((inPoint.rect.center + outPoint.rect.center) * 0.5f, Quaternion.identity, 4, 8, Handles.RectangleHandleCap)) {
+          if (OnClickRemoveConnection != null) {
+            OnClickRemoveConnection(this);
+          }
+        }
+      }
+    }
+  }
+}
+
---
+++ scripts/Editor/WowNodeEditor.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Wowsome {
+  namespace Node {
+    public delegate void Ev();
+    public delegate void EvWithParam<E>(E param);
+
+    public interface INodeEditor {
+      EvWithParam<WowNode> OnRemoveNode { get; set; }
+    }
+
+    public class WowNodeEditor<T> : EditorWindow, INodeEditor where T : WowNode {
+      public delegate T AddNode(Vector2 pos);
+
+      List<WowNode> _nodes = new List<WowNode>();
+
+      Vector2 _offset;
+      Vector2 _drag;
+
+      public AddNode OnAddNode { get; set; }
+      public EvWithParam<WowNode> OnRemoveNode { get; set; }
+
+      protected virtual void OnEnable() {
+        OnRemoveNode += node => _nodes.Remove(node);
+      }
+
+      void OnGUI() {
+        DrawGrid(20, 0.2f, Color.gray);
+        DrawGrid(100, 0.4f, Color.gray);
+        DrawNodes();
+        ProcessNodeEvents(Event.current);
+        ProcessEvents(Event.current);
+        if (GUI.changed) Repaint();
+      }
+
+      void DrawGrid(float gridSpacing, float gridOpacity, Color gridColor) {
+        int widthDivs = Mathf.CeilToInt(position.width / gridSpacing);
+        int heightDivs = Mathf.CeilToInt(position.height / gridSpacing);
+
+        Handles.BeginGUI();
+        Handles.color = new Color(gridColor.r, gridColor.g, gridColor.b, gridOpacity);
+
+        _offset += _drag * 0.5f;
+        Vector3 newOffset = new Vector3(_offset.x % gridSpacing, _offset.y % gridSpacing, 0);
+
+        for (int i = 0; i < widthDivs; i++) {
+          Handles.DrawLine(new Vector3(gridSpacing * i, -gridSpacing, 0) + newOffset, new Vector3(gridSpacing * i, position.height, 0f) + newOffset);
+        }
+
+        for (int j = 0; j < heightDivs; j++) {
+          Handles.DrawLine(new Vector3(-gridSpacing, gridSpacing * j, 0) + newOffset, new Vector3(position.width, gridSpacing * j, 0f) + newOffset);
+        }
+
+        Handles.color = Color.white;
+        Handles.EndGUI();
+      }
+
+      void DrawNodes() {
+        for (int i = 0; i < _nodes.Count; i++) {
+          _nodes[i].Draw();
+        }
+      }
+
+      void ProcessEvents(Event e) {
+        _drag = Vector2.zero;
+
+        switch (e.type) {
+          case EventType.MouseDown:
+            // right click
+            if (e.button == 1) {
+              OnRightClick(e.mousePosition);
+            }
+            break;
+
+          case EventType.MouseDrag:
+            if (e.button == 0) {
+              OnDrag(e.delta);
+            }
+            break;
+        }
+      }
+
+      void ProcessNodeEvents(Event e) {
+        foreach (var node in _nodes) {
+          bool guiChanged = node.ProcessEvents(e);
+          if (guiChanged) {
+            GUI.changed = true;
+          }
+        }
+      }
+
+      void OnRightClick(Vector2 mousePosition) {
+        GenericMenu genericMenu = new GenericMenu();
+        genericMenu.AddItem(new GUIContent("Add node"), false, () => {
+          T node = OnAddNode?.Invoke(mousePosition);
+          _nodes.Add(node);
+        });
+        genericMenu.ShowAsContext();
+      }
+
+      void OnDrag(Vector2 delta) {
+        _drag = delta;
+        _nodes.ForEach(node => node.Drag(delta));
+        GUI.changed = true;
+      }
+    }
+  }
+}
+
